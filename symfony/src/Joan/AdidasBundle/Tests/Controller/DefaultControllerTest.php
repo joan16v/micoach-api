cb386f1675853ea157061a7b1c2789fe89cc6e43
Joan Gimenez Donat <joan16v@gmail.com>
2017-10-06 08:01:04 +0200

    Update DefaultControllerTest.php

--- symfony/src/Joan/AdidasBundle/Tests/Controller/DefaultControllerTest.php
+++ symfony/src/Joan/AdidasBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,9 +9,7 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
         $this->assertContains('Hello World', $client->getResponse()->getContent());
     }
 }
